72f40ba80c4f5b80ff22c81e0bbfe1dcbdf17de8
kegan <keganferdinand@gmail.com>
2023-10-24 15:31:46 -0400

    Initial project setup, implemented Add method in Program.cs

---
+++ Reminders/Program.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleIOManager;
+using RemindersLib;
+using static System.Console;
+namespace Program;
+
+//-----------------------------V1.1-KeganF------------------------------//
+// CLASS > Program                                                      //
+//         Contains the Main method for the Reminders Console App       //
+//         Allows users to manage their reminders via the console       //
+//----------------------------------------------------------------------//
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Add();
+    }
+
+    //----------------------------------------------------------------------//
+    // METHOD > Add                                                         //          
+    //          Creates a new Reminder object constructed with values given //
+    //          by the user                                                 //             
+    //                                                                      //
+    // PARAMS > none                                                        //
+    //                                                                      //
+    // RETURN > void                                                        //
+    //----------------------------------------------------------------------//
+    public static void Add()
+    {
+        // Set title and description
+        Write("Set a title for your reminder > ");
+        string title = ReadLine() ?? "";
+        Write("Set a description for your reminder > ");
+        string desc = ReadLine() ?? "";
+
+        // Set date and time
+        DateOnly date = DateOnly.FromDateTime(
+            ConsoleManager.GetConvertedInput<DateTime>("Set a date for this reminder "));
+        TimeOnly time = TimeOnly.FromDateTime(
+            ConsoleManager.GetConvertedInput<DateTime>("Set a time for this reminder "));
+
+        // Check for dates in the past
+        if (DateOnly.FromDateTime(DateTime.Now) > date)
+        {
+            bool isAcceptedDate = Convert.ToBoolean(
+                ConsoleManager.DisplayCheckBoxes(new string[]{"No", "Yes"},
+                $"{date} has already past. Do you want to finish creating this reminder?"));
+
+            if (!isAcceptedDate)
+            {
+                return;
+            }
+        }
+        
+        bool isAutoDel = Convert.ToBoolean(
+            ConsoleManager.DisplayCheckBoxes(new string[]{"No", "Yes"}, 
+            $"Do you want this reminder to be automatically deleted after {date} at {time}?"));
+
+        Reminder r = new Reminder(title, desc, date, time, isAutoDel);
+        r.DisplaySummary();
+    }
+}
---
+++ RemindersLib/Reminder.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleIOManager;
+using static System.Console;
+namespace RemindersLib;
+//-----------------------------Constructors-----------------------------//
+//-----------------------------V1.1-KeganF------------------------------//
+// CLASS > Reminder                                                     //
+//         Provides members for defining Reminder objects               //
+//----------------------------------------------------------------------//
+public class Reminder
+{
+    //---------------------------v-Constructors-v---------------------------//
+    public Reminder(string title, string desc, 
+        DateOnly date, TimeOnly time, bool isAutoDel)
+    {
+        Title     = title;
+        Desc      = desc;
+        Date      = date;
+        Time      = time;
+        IsAutoDel = isAutoDel;
+    }
+    
+    //-----------------------------v-Methods-v------------------------------//
+    //----------------------------------------------------------------------//
+    // METHOD > DisplaySummary                                              //          
+    //          Formats and displays object values to quickly summarize     //
+    //          a Reminder                                                  //
+    //                                                                      //
+    // PARAMS > none                                                        //
+    //                                                                      //
+    // RETURN > void                                                        //
+    //----------------------------------------------------------------------//
+    public void DisplaySummary()
+    {
+        WriteLine($"Title: {Title}");
+        WriteLine($"Desc: {Desc}");
+        WriteLine($"Date: {Date}");
+        WriteLine($"Time: {Time}");
+        if (ExpiredDays > 0)
+        {
+            ConsoleManager.WriteColoredLine(
+                $"This reminder expired {ExpiredDays} ago!", ConsoleColor.Red);
+        }
+    }
+    
+    //----------------------------v-Properties-v----------------------------//
+    public string Title   { get; set; }
+    public string Desc    { get; set; }
+    public DateOnly Date  { get; set; }
+    public TimeOnly Time  { get; set; }
+    public bool IsAutoDel { get; set; }
+    
+    public int ExpiredDays 
+    { 
+        get 
+        { 
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+            return today.DayNumber - Date.DayNumber;
+        }
+    }
+
+}
